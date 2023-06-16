4c763da383e903a778ee50cbb5b4d0f5211962f2
Sandro <wrek.yamom@gmail.com>
2023-06-16 16:25:05 +0400

    add update page

--- app.py
+++ app.py
@@ -31,3 +31,41 @@ class Event(db.Model):
             'age_restriction': self.age_restriction,
 
         }
+
+
+
+@app.route('/update', methods=['GET', 'PUT', 'POST'])
+def update():
+    id = request.args.get('id')
+    event = Event.query.filter(Event.id==id).first()
+    form = Events()
+    if form.validate_on_submit():
+        event_name = form.event_name.data
+        description = form.description.data
+        category = form.category.data
+        holding_time = form.holding_time.data
+        location = form.location.data
+        age_restriction = form.age_restriction.data
+        if event is not None:
+            event.event_name = event_name
+            event.description = description
+            event.category = category
+            event.event_name = event_name
+            event.holding_time = holding_time
+            event.location = location
+            event.age_restriction = age_restriction
+            db.session.commit()
+        return redirect(url_for('updated'))
+    return render_template('update.html', form=form, event=event)
+
+
+@app.route('/updated')
+def updated():
+    massage = f"Successfully updated"
+    return render_template('success page.html', massage=massage)
+
+
+
+
+with app.app_context():
+    db.create_all()
